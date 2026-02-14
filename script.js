f03dc3af5a811011bbcc31c87f65c8f80b7eacfe
cmouchiee-stack <cmouchiee@gmail.com>
2026-02-15 01:40:53 +0700

    Add files via upload
    pelentin jir

---
+++ script.js
@@ -0,0 +1,60 @@
+onload = () => {
+    document.body.classList.remove("container");
+  };
+
+  const wrapper = document.querySelector(".wrapper");
+  const openBtn = document.getElementById("openBtn");
+  const closeBtn = document.getElementById("closeBtn");
+  
+  openBtn.addEventListener("click", () => {
+      wrapper.classList.add("open");
+      openBtn.style.display = "none";
+      closeBtn.style.display = "inline-block";
+  });
+  
+  closeBtn.addEventListener("click", () => {
+      wrapper.classList.remove("open");
+      closeBtn.style.display = "none";
+      openBtn.style.display = "inline-block";
+  });
+  const container = document.querySelector(".hearts-container");
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = (Math.random() * 3 + 3) + "s";
+    heart.style.opacity = Math.random();
+
+    container.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 6000);
+}
+
+setInterval(createHeart, 200);
+const intro = document.querySelector(".intro");
+const mainContent = document.querySelector(".main-content");
+const startBtn = document.querySelector(".startBtn");
+
+startBtn.addEventListener("click", () => {
+    intro.style.display = "none";
+    mainContent.classList.add("show");
+});
+window.addEventListener("DOMContentLoaded", () => {
+    const text = "SEREIYA";
+    const typingElement = document.getElementById("typingName");
+    let index = 0;
+
+    function typeEffect() {
+        if (index < text.length) {
+            typingElement.innerHTML += text.charAt(index);
+            index++;
+            setTimeout(typeEffect, 120);
+        }
+    }
+
+    typeEffect();
+});
